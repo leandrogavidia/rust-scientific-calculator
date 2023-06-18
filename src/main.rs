bca871046e3c0ce8be1f354ba55e2567d87bdc00
leandrogavidia <leandergs1234@gmail.com>
2023-06-18 17:56:21 -0400

    feat: Allow floating numbers in the operations. Allow positive and negative signs at the beginning of the operation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 
 fn create_regex(operator: &str) -> Regex {
-    let regex_structure = format!(r"(\d+)\s?\{operator}\s?(\d+)");
+    let regex_structure = format!(r"([+-]?\d+(\.\d+)?)\s?\{operator}\s?(\d+(\.\d+)?)");
     let regex = Regex::new(&regex_structure).unwrap();
     return regex
 }
@@ -15,14 +15,14 @@ fn resolve_operation(mut operation: String, operator: &str) -> String {
         
         let caps = caps.unwrap();
         let caps_operation = caps.get(0).unwrap().as_str();
-        let left_number: i128 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_number: i128 = caps.get(2).unwrap().as_str().parse().unwrap();
+        let left_number: f64 = caps.get(1).unwrap().as_str().parse::<f64>().unwrap();
+        let right_number: f64 = caps.get(3).unwrap().as_str().parse::<f64>().unwrap();
         let result = match operator {
             "*" => left_number * right_number,
             "/" => left_number / right_number,
             "+" => left_number + right_number,
             "-" => left_number - right_number,
-            _ => 0
+            _ => 0.0
         };
         operation = operation.replace(caps_operation, &result.to_string());
     }
